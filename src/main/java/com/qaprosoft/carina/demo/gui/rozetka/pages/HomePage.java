7623d97810fa74d12c27c219b2729d1554838276
BairamovM <muslimbairamov@gmail.com>
2023-01-14 21:31:06 +0200

    Add HomePage Rozetka

---
+++ src/main/java/com/qaprosoft/carina/demo/gui/rozetka/pages/HomePage.java
@@ -0,0 +1,13 @@
+package com.qaprosoft.carina.demo.gui.rozetka.pages;
+
+import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends AbstractPage {
+
+
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+}
