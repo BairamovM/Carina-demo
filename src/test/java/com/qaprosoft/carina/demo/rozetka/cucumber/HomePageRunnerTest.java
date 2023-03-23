7b309c3aeb9510f39e9782602bcc633673ea8404
BairamovM <muslimbairamov@gmail.com>
2023-03-24 00:27:14 +0200

    Add json reports

--- src/test/java/com/qaprosoft/carina/demo/rozetka/cucumber/HomePageRunnerTest.java
+++ src/test/java/com/qaprosoft/carina/demo/rozetka/cucumber/HomePageRunnerTest.java
@@ -4,7 +4,8 @@ import com.qaprosoft.carina.core.foundation.cucumber.CucumberRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "@HomePage", features = {"src/test/resources/features/rozetka/HomePage.feature"})
+@CucumberOptions(tags = "@HomePage", features = {"src/test/resources/features/rozetka/HomePage.feature"},
+        plugin = {"pretty", "json:target/cucumber_reports/home_page.json"})
 public class HomePageRunnerTest extends AbstractTestNGCucumberTests {
 
 }
