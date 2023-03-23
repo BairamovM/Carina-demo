ce96b3c0b2288772b6f548454938511a3bbe3aed
BairamovM <muslimbairamov@gmail.com>
2023-03-23 18:10:05 +0200

    fix

--- src/test/java/com/qaprosoft/carina/demo/rozetka/cucumber/HomePageRunnerTest.java
+++ src/test/java/com/qaprosoft/carina/demo/rozetka/cucumber/HomePageRunnerTest.java
@@ -1,10 +1,9 @@
 package com.qaprosoft.carina.demo.rozetka.cucumber;
 
-import com.qaprosoft.carina.core.foundation.cucumber.CucumberBaseTest;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
+import com.qaprosoft.carina.core.foundation.cucumber.CucumberRunner;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags = "@HomePage", features = {"src/test/resources/features/rozetka/HomePage.feature"})
-public class HomePageRunnerTest extends AbstractTestNGCucumberTests {
+public class HomePageRunnerTest extends CucumberRunner {
 
 }
